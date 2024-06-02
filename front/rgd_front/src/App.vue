af8fe148d0fd758a4774fc206e6928bc87f3b298
Виталий Кодолов <vitalykodolov@yandex.ru>
2024-06-02 07:47:53 +0500

    add

--- front/rgd_front/src/App.vue
+++ front/rgd_front/src/App.vue
@@ -10,8 +10,8 @@ const Item_get = (id) => {
 </script>
 
 <template>
-  <nav class="navbar navbar-expand-sm bg-dark back">
-    <div class="container-fluid">
+  <nav class="navbar navbar-expand-sm back ">
+    <div class="container-fluid justify-content-center">
       <ul class="navbar-nav">
         <li class="nav-item">
           <a class="nav-link active" href="#"><img src="./assets/RZD logo.png" alt="" width="300px" height="50px"></a>
